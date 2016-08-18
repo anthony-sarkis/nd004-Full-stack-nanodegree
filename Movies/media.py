105bf581112d141a101abaff7cd235d577c29809
Anthony <anthonysarkis@gmail.com>
2016-08-18 17:40:00 -0600

    Lots of learnings

--- Movies/media.py
+++ Movies/media.py
@@ -1,14 +1,28 @@
 
 import webbrowser
 
-class Movie():
-    def __init__(self, title, storyline, poster_img, trailer_url):
+class Video():
+    #Parent class
+    def __init__(self, title):
         self.title = title
+
+class TvShow(Video):
+    #Child class
+    def __init__(self, title): 
+        Video.__init__(self, title)
+        self.season = season
+        self.episode = episode
+        self.tv_station = tv_station
+
+class Movie(Video):
+    """ DOCS """
+    valid_ratings = ["G", "PG", "R"]
+
+    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+        Video.__init__(self, title)
         self.storyline = storyline
-        self.poster_img = poster_img
-        self.trailer_url = trailer_url
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
 
     def show_trailer(self):
         webbrowser.open(self.trailer_url)
-
-   
---
+++ inheritance.py
@@ -0,0 +1,31 @@
+
+#in same file for ease of demo
+class Parent():
+    def __init__(self, last_name, eye_color):
+        print("Parent Constructor Called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+    def show_info(self):
+        print("Last name - "+self.last_name)
+        print("Eye color - "+self.eye_color)
+
+class Child(Parent):
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child Constructor Called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+
+    def show_info(self):
+        print("Last name - "+self.last_name)
+        print("Eye color - "+self.eye_color)
+        print("Number of toys - "+str(self.number_of_toys))
+        
+#billy_cryus = Parent("Cryus", "blue")
+#billy_cryus.show_info()
+
+miley_cryus = Child("Cyrus", "Blue", 5)
+#print(miley_cryus.last_name)
+#print(miley_cryus.number_of_toys)
+
+miley_cryus.show_info()
