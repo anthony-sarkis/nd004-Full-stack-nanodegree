0aad98e8d15e330c4490158fa9b59b84185ebc13
swirlingsand <anthonysarkis@gmail.com>
2016-08-24 23:21:02 -0600

    templates, jinja2, debugging

--- anthony-app-engine/templates/main.py
+++ anthony-app-engine/templates/main.py
@@ -4,22 +4,8 @@ import webapp2
 import jinja2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileStyemLoader(template_dir))
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
-hidden_html = """
-    <input type="hidden" name="food" value="%s">
-"""
-
-item_html = "<li>%s</li>"
-
-shopping_list_html = """
-        <br>
-        <br>
-    <h2>Shopping List</h2>
-        <ul>
-        %s
-        </ul>
-"""
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -29,12 +15,14 @@ class Handler(webapp2.RequestHandler):
         t = jinja_env.get_template(template)
         return t.render(params)
 
-    def redner(self, template, **kw):
+    def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class MainHandler(webapp2.RequestHandler):
+
+class MainHandler(Handler):
     def get(self):
-        self.render("shopping_list_html")
+        #self.response.out.write("Thanks!")
+        self.render("shopping_list.html")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
