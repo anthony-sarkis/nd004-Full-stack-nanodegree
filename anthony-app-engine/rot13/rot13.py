e67b069a6546c2494b40b4b6f92038bfd197eedd
swirlingsand <anthonysarkis@gmail.com>
2016-09-17 15:56:46 -0600

    Finished Rot13 and starter blog entry
    Test and passed udacity grader. Really happy with where it's at. Used
    DIV instead of TD like they did ;) Still need to understand some of the
    sub functions / layout a little better. / Study syntax further.

--- anthony-app-engine/rot13/rot13.py
+++ anthony-app-engine/rot13/rot13.py
@@ -40,7 +40,79 @@ class Rot13(Handler):
 		# render HTML form and pass rot13 encoded text to text varilable
 		self.render('rot13-form-custom.html', text = rot13)
 		
+# Define error handling functions
+
+# Allow a-z, A-Z, 0-0, _, - using regular expression
+# 3 - 20 characters
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+# Check password
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+# Check email
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return email and EMAIL_RE.match(email)
+
+# Render welcome page if user has valid username, else redirect to signup
+class Welcome(Handler):
+    def get(self):
+        # Get username so we can use it as local var
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username = username)
+        else:
+            self.redirect('/signup')
+
+class Signup(Handler):
+
+    # Render basic html page
+    def get(self):
+        self.render("blog-sign-up-form.html")
+
+    # Post logic
+    def post(self):
+        # have_error var is used to determine if we should redirect or not
+        have_error = False
+
+        # get all vars
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        # create dictionary for error handling
+        params = dict(username = username,
+                        email = email)
+
+        # error handling
+        if not valid_username(username):
+            params['error_username'] = "Invalid username"
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "Invalid password"
+            have_error = True
+
+        elif password != verify:
+            params['error_verify_password'] = "Passwords don't match"
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "Invalid email"
+            have_error = True
+
+        if have_error:
+            self.render('blog-sign-up-form.html', **params)
+        else:
+            self.redirect('/welcome?username=' + username)
 
 app = webapp2.WSGIApplication([
     ('/', Rot13),
+    ('/signup', Signup),
+    ('/welcome', Welcome),
 ], debug=True)
