d680cb63b3ceace54b608f60cbb546b91fa48df3
Anthony <anthonysarkis@gmail.com>
2017-01-30 17:06:02 -0600

    catclicker premium pro
    issues with syncing without page refresh. need to look into "this.", and
    how the render functions are access the current cat.

--- javascript-design-patterns/cat-clicker/js/script.js
+++ javascript-design-patterns/cat-clicker/js/script.js
@@ -12,20 +12,21 @@ $(function(){
     var model = {
       
       init: function () {
+        currentCat = null,
         cats = [
           {
             Name: 'Whiskers',
-            Img: 'a',
+            ImageURL: 'Whiskers.jpg',
             Count: 0
           },
           {
             Name: 'Symba',
-            Img: 'a',
+            ImageURL: 'Symba.jpg',
             Count: 0
           },
           {
             Name: 'Kitty',
-            Img: 'a',
+            ImageURL: 'Kitty.jpg',
             Count: 0
           }
               ]
@@ -33,6 +34,7 @@ $(function(){
         // push local storage if doesn't already exist
       if (!localStorage.cats) {
             localStorage.cats = JSON.stringify(cats);
+            localStorage.currentCat = currentCat;
         } 
       },        
       
@@ -51,15 +53,42 @@ $(function(){
         var originalCat = model.getCat(catToBeUpdated);
         console.log("original cat", originalCat)
 
-        // Update values in orginal cat
-        // This is so it only updates the one field at a time
-        originalCat.count = catToBeUpdated.count
-        // console.log(originalCat)
+        originalCat.Count = Number(catToBeUpdated.Count)
+          
+        //console.log(catToBeUpdated.Count)
+        //console.log(Number(catToBeUpdated.Count))
 
         // Update cat
         for (key in Cats) {
           if (originalCat.Name == Cats[key].Name) {
               Cats[key] = originalCat
+          }
+        }
+
+        localStorage.cats = JSON.stringify(Cats);
+      },
+
+            /* 
+
+        Purpose: Updates Cats array of objects in local storage
+        Input: Cat object with variable updates
+        Returns: None.
+
+      */
+      updateCatAll: function(cat) {
+        
+        // get cats
+        var catToBeUpdated = cat
+        var Cats = model.getCatAll();
+        var originalCat = model.getCat(catToBeUpdated);
+        console.log("original cat", originalCat)
+
+        catToBeUpdated.Count = Number(catToBeUpdated.Count)
+
+        // Update cat
+        for (key in Cats) {
+          if (originalCat.Name == Cats[key].Name) {
+              Cats[key] = catToBeUpdated
           }
         }
 
@@ -97,7 +126,20 @@ $(function(){
             }
           };
         }
+
+        localStorage.currentCat = catName;
+        console.log('local storage update', localStorage.currentCat)
+
         return cat;
+      },
+
+
+      getCurrentCat: function() {
+
+        var currentCat = localStorage.currentCat;
+        console.log("cat from storage", currentCat)
+        return currentCat;
+
       }
     };
     
@@ -132,6 +174,36 @@ $(function(){
         });
       },
 
+
+      /* 
+
+        Purpose: Save cat
+        Input: Form data from Admin
+        Returns: Updated cat in local storage
+
+      */
+
+      saveCat: function(catName) {
+        
+        $('form').bind('submit', function (event) {
+
+          event.preventDefault(); // using this page stop being refreshing
+
+          // credit http://stackoverflow.com/questions/169506/obtain-form-input-fields-using-jquery
+          var result = { };
+          $.each($('form').serializeArray(), function() {
+              result[this.name] = this.value;
+          });
+
+          console.log(result);
+          
+          model.updateCatAll(result);
+
+          console.log("Save Clicked");        
+
+        });
+      },
+
       /* 
 
         Purpose: Show a cat and count it's clicks
@@ -142,25 +214,29 @@ $(function(){
 
       showCat: function() {
         
-        $(document).click(function(e) {
+        $('#cat-list').click(function(e) {
 
           parantNodeId = e.target.parentNode.id
           if(parantNodeId == 'cat-list') {
             
-            var cat = $(e.target).text();
-            console.log(cat)
+            var catName = $(e.target).text();
+            var cat = model.getCat(catName);
 
-            view.renderCat(cat);
-            octopus.countCat(cat);
+            view.renderCat(catName);
+            octopus.countCat(catName);
+            view.renderAdmin(catName);
+
           };
         });
       },
 
+
       init: function() {
         model.init()
-        view.init()
+        view.init()        
       }
     };
+
 
 
     var view = {
@@ -178,15 +254,54 @@ $(function(){
 
       renderCat: function(catName) {
           
-          var cat = model.getCat(catName);
-          
-          // clear old cat
-          $('#cats').find('p').first().remove();
-          $('#cats').find('img').first().remove(); 
+        var cat = model.getCat(catName);
+        // clear old cat
+        // TO DO update logic to be use replaceWith function
+        $('#cats').find('p').first().remove();
+        $('#cats').find('img').first().remove();
+        $('<p>').attr('id', catName+"Output").appendTo('#cats').html('a');
+        $('<img>').attr('src', cat.ImageURL).attr('id', catName+"Image").appendTo('#cats');
 
-          $('<p>').attr('id', catName+"Output").appendTo('#cats').html('a');
-          $('<img>').attr('src', catName+".jpg").attr('id', catName+"Image").appendTo('#cats');
       },
+
+      /* 
+
+        Purpose: Render Admin form
+        Input: 
+        Returns: 
+
+      */
+
+      renderAdmin: function(catName) {
+        
+        var cat = model.getCat(catName); 
+        var selectACat = 0;
+        var fillForm = [];
+        fillForm.push(cat.Name, cat.ImageURL, cat.Count);
+
+        /* 
+        Finds AdminForm ID, then for each input field, 
+        if the inputType is not a submit type, it iterates over each
+        It then replaces the value of the input tag where name is equal to name in loop,
+        with the value attribute from the array specified above.
+        */
+
+        // credit http://stackoverflow.com/questions/5603117/jquery-create-object-from-form-fields
+        $("#AdminForm").find("input, text").each(function() {
+          var inputType = this.tagName.toUpperCase() === "INPUT" && this.type.toUpperCase();
+          
+          if (inputType !== "SUBMIT") {
+              
+              console.log(this.name)
+              $('').replaceWith($("input[name='"+this.name+"']").attr('value', fillForm[selectACat]));
+              selectACat++;
+
+          }
+        });
+
+        octopus.saveCat(cat.Name);
+      },
+
 
       /* 
 
@@ -197,6 +312,9 @@ $(function(){
       */
 
       buildCatList: function() {
+        
+        $('#cat-list').find('li').remove();
+
         // run through list of cats
         var Cats = model.getCatAll();
 
@@ -206,12 +324,20 @@ $(function(){
             cat = Cats[cat]
             var li = $('<li>').attr('id', cat.Name).html(cat.Name)
             li.appendTo('#cat-list');
+            
+            // $('#cat-list').replaceWith($('<li>').attr('id', cat.Name).html(cat.Name));
           }
         };
       }
+
+
     };
 
     octopus.init();
     octopus.showCat();
+
+    // view.renderCat();
+    // octopus.countCat();
+    // view.renderAdmin();
 
 });
